c61aac14db0b06739f60cbe334ea412a8f8d6eb0
ahmed shahwan <shahwanahmed15@outlook.com>
2020-10-01 13:21:58 +0200

    ahmed shahwan

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-'use strict'
+'use strict';
+const movieAPI = `http://www.omdbapi.com/?t=${title}&apikey=e6a3e2df`;
+async function main() {
+    const response = await fetch(movieAPI);
+    const data = await response.json();
 
-/*
-    Put your JavaScript here
-*/
+    createMovieList(data);
+}
+main();
+
+function createMovieList(data) {
+    const title = data.Title;
+    const img = data.Poster;
+    const plot = data.Plot;
+    const selector = document.getElementById('selector');
+    selector.addEventListener('change', function(e) {
+        if (e.target.value === option.name) {
+            const display = document.getElementById('display');
+            display.innerHTML = `
+    <h1>${title}</h1>
+    <img src ='${img}' alt=''>
+    <p>${plot}</p>
+    `;
+        }
+    });
+}
